44a048aa27577c3c0a8fc1844f8367ac00fab4e9
janetmndz <janetmendez.03@gmail.com>
2019-07-21 16:28:03 -0400

    Done.

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,34 @@
 class Mechanic
 
+  @@all = []
+
   attr_reader :name, :specialty
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
 
+    @@all << self
+  end
+
+  def self.all
+    ## Gets list of all mechanics
+    @@all
+  end
+
+  def cars
+    ## Gets list of all cars a mechanic services
+    Car.all.select{|car| car.mechanic == self}
+  end
+
+  def car_owners
+    ## Gets list of all CarOwner instances that hire this mechanic
+    self.cars.map{|car| car.owner}.uniq
+  end
+
+  def car_owners_names
+    ## Gets list of all car owner names this mechanic services
+    self.car_owners.map{|owner| owner.name}
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,25 @@ def reload
   load 'config/environment.rb'
 end
 
+## Testing 
+adam = CarOwner.new("Adam")
+dax = CarOwner.new("Dax")
+millers = CarOwner.new("Miller")
+
+dave = Mechanic.new("Dave", "antique")
+claire = Mechanic.new("Claire", "exotic")
+erin = Mechanic.new("Erin", "clunker")
+
+car1 = Car.new("Honda", "Civic", "antique", adam, dave)
+car2 = Car.new("Honda", "Civic", "antique", dax, dave)
+car3 = Car.new("Honda", "Civic", "antique", adam, dave)
+car4 = Car.new("Ferrari", "458", "exotic", dax, claire)
+car5 = Car.new("McLaren", "570S", "exotic", dax, claire)
+car6 = Car.new("Ford", "2009", "clunker",adam, erin)
+car7 = Car.new("Ford", "2009", "clunker", millers, erin)
+car8 = Car.new("Ferrari", "453", "exotic", millers, claire)
+
+
 binding.pry
+
+puts "KA CHOW!"
