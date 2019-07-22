9d9cdd19a99065b8791f4562aa8090a4f1a36f22
vertreesa <Alessandra.Vertrees82@myhunter.cuny.edu>
2019-07-22 09:42:52 -0400

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,39 @@
 class CarOwner
+=begin 
+**CarOwner**
 
+  - Get a list of all owners X
+
+  - Get a list of all the cars that a specific owner has 
+
+  - Get a list of all the mechanics that a specific owner goes to
+
+  - Get the average amount of cars owned for all owners
+=end
   attr_reader :name
-
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 
+  def cars
+    Car.all.select do |car|
+      car.car_owner == self
+    end
+  end
+
+  def mechanics
+    self.cars.map do |car|
+      car.mechanic
+    end
+  end
+
+  def self.average_car_num
+    Car.all.count / self.all.count
+  end
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,44 @@
 class Mechanic
+=begin
+**Mechanic**
+
+  - Get a list of all mechanics X
+
+  - Get a list of all cars that a mechanic services X
+
+  - Get a list of all the car owners that go to a specific mechanic X
+
+  - Get a list of the names of all car owners who
+  go to a specific mechanic 
+=end
 
   attr_reader :name, :specialty
+  @@all = []
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  def self.all
+    @@all
+  end
 
+  def cars
+    Car.all.select do |car|
+      car.mechanic == self
+    end
+  end
 
+  def car_owners 
+    self.cars.map do |car|
+      car.car_owner
+    end
+  end
+
+  def car_owner_names
+    self.cars_owners.map do |car|
+      car.car_owner.name
+    end
+  end
 end
