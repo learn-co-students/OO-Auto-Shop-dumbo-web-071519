bd1d0fe26ae43ca25bb4234455957008db01a777
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-07-21 22:29:48 -0400

    finished lab

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,28 @@
 class Car
 
-  attr_reader :make, :model
+  @@all = []
 
-  def initialize(make, model, classification)
+  attr_reader :make, :model, :classification, :owner, :mechanic
+
+  def initialize(make, model, classification, owner, mechanic)
     @make = make
     @model = model
     @classification = classification
+    @owner = owner
+    @mechanic = mechanic
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.classifications
+    self.all.map{|car| car.classification}.uniq
+  end
+
+  def mechanics_with_classification
+   cars_by_specialty = Car.all.select{|car| self.classification == car.mechanic.specialty}
+   cars_by_specialty.map{|car| car.mechanic}
+  end
 end
--- app/models/run.rb
+++ app/models/run.rb
@@ -1,8 +1,7 @@
-require_relative '../config/environment.rb'
-
-def reload
-  load 'config/environment.rb'
-end
+require "./car_owner.rb"
+require "./car.rb"
+require "./mechanic.rb"
+require "pry"
 
 co1 = CarOwner.new("jim")
 co2 = CarOwner.new("bob")
@@ -15,8 +14,12 @@ m3 = Mechanic.new("baz", "sedan")
 m4 = Mechanic.new("ooof", "sports")
 
 ca1 = Car.new("Honda","1","sedan",co1, m3)
-ca2 = Car.new("Mazda","2","SUV",co1, m2)
+ca2 = Car.new("Mazda","2","suv",co1, m2)
 ca3 = Car.new("Hyundai","1","sedan",co2, m1)
 ca4 = Car.new("Ferarri","4","sports",co3, m4)
 
+
 binding.pry
+puts "Vroom"
+
+
