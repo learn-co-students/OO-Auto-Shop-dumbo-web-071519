8d0c1ec165b49df203354a495ab80265183e150a
Ross Perkel <rossperkel@gmail.com>
2019-07-22 08:37:53 -0400

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -20,6 +20,12 @@ class CarOwner
     self.get_cars.map{|car| car.mechanic}
   end
 
-  
+  def self.avg_cars_per_owner
+  #   car_counts = self.all.map{|owner| owner.get_cars.count}
+  #   avg = (car_counts.inject(0){|sum, i| sum+i})/car_counts.size
+    Car.all.count.to_f/CarOwner.all.count
+end
+
+
 
 end
